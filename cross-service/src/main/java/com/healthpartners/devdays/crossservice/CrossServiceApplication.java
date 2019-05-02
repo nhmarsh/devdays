14d626c9d2e08425789a8944b7213daea44d9d6c
Nicholas Marsh <nicholas.h.marsh@healthpartners.com>
2019-05-02 13:43:52 -0500

    This is demoable

--- cross-service/src/main/java/com/healthpartners/devdays/crossservice/CrossServiceApplication.java
+++ cross-service/src/main/java/com/healthpartners/devdays/crossservice/CrossServiceApplication.java
@@ -3,9 +3,11 @@ package com.healthpartners.devdays.crossservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class CrossServiceApplication {
 
 	public static void main(String[] args) {
--- cross-service/src/main/java/com/healthpartners/devdays/crossservice/service/CommentService.java
+++ cross-service/src/main/java/com/healthpartners/devdays/crossservice/service/CommentService.java
@@ -1,12 +1,14 @@
 package com.healthpartners.devdays.crossservice.service;
 
 import com.healthpartners.devdays.crossservice.dto.CommentDto;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -15,12 +17,14 @@ public class CommentService {
     private final String commentServiceUrl = "http://localhost:8080/comment/";
     private final ParameterizedTypeReference<List<CommentDto>> parameterizedTypeReference = new ParameterizedTypeReference<List<CommentDto>>() {};
 
+    @HystrixCommand(fallbackMethod = "fallbackGetAllComments")
     public List<CommentDto> getAllComments() {
         ResponseEntity<List<CommentDto>> responseEntity = restTemplate.exchange(commentServiceUrl , HttpMethod.GET, null, parameterizedTypeReference);
         List<CommentDto> commentDtoList = responseEntity.getBody();
         return commentDtoList;
     }
 
+    @HystrixCommand(fallbackMethod = "fallbackGetCommentById")
     public CommentDto getCommentById(Long id) {
         String commentUrl = commentServiceUrl + id;
         //todo error handling
@@ -28,5 +32,14 @@ public class CommentService {
 
         return commentDto;
     }
+
+    public List<CommentDto> fallbackGetAllComments() {
+        return Arrays.asList(CommentDto.builder().comment("Comment service is down. Please try again later").build());
+    }
+
+    public CommentDto fallbackGetCommentById(Long id) {
+        return CommentDto.builder().comment("Comment service is down. Please try again later").build();
+    }
+
 
 }
